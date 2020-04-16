26d42f2fc6468ebc60013dddb460113e49de95b8
DavidVargas1213 <47476556+DavidVargas1213@users.noreply.github.com>
2020-04-16 11:56:54 -0500

    Servidor web corriendo correctamente
    Revision y analisis del codigo

--- ServidorHTTP/src/httpserver.java
+++ ServidorHTTP/src/httpserver.java
@@ -11,118 +11,83 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.StringTokenizer;
 
-
 public class httpserver implements Runnable {
-	
-	static final File WEB_ROOT = new File(".");
-	static final String DEFAULT_FILE = "index.html";
-	static final String ERROR_NOT_FOUND = "Error_404";
-	static final String METHOD_NOT_SUPPORTED = "not_supported.html";
-	
-	static final int PORT = 8680;
-	
+
+	static final File WEB_ROOT = new File(".");  
+	static final String INDEX = "index.html";
+	static final String ERROR_NOT_FOUND = "Error_404.html";
+
+	static final int PORT = 80;
 	static final boolean verbose = true; 
-	
 	private Socket connect;
-	
+
 	public httpserver(Socket a) {
 		connect = a; 
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			ServerSocket servidorConectado = new ServerSocket(PORT);
 			System.out.println("Servidor iniciado");
-			
+
 			while(true) {
 				httpserver myServer = new httpserver(servidorConectado.accept());
-				
+
 				if(verbose) {
 					System.out.println("Conexion abierta");
 				}
-				
+
 				Thread thread = new Thread(myServer);
 				thread.start();
-				
-				
 			}
 		} catch (IOException e) {
-			System.err.println("Error de conexion con el servidor : " + e.getMessage()); 
+			System.err.println("Puerto 80 Ocupado");
 		}
-		
 	}
-	
+
 	public void run() {
-		
+
 		BufferedReader in = null; 
 		PrintWriter out = null;
 		BufferedOutputStream  dataOut = null; 
 		String fileRequested = null;
-		
+
 		try {
 			in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-			
 			out = new PrintWriter(connect.getOutputStream());
-			
 			dataOut = new BufferedOutputStream(connect.getOutputStream());
-			
+
 			String input = in.readLine();
-			
 			StringTokenizer parse = new StringTokenizer(input);
-			String method = parse.nextToken().toUpperCase();
-			
+			String mayus = parse.nextToken().toUpperCase();
 			fileRequested = parse.nextToken().toLowerCase();
-			
-			
-			if(!method.equals("GET") && !method.equals("HEAD")) {
-				if (verbose) {
-					System.out.println("501 :" + method + "method.");
-				}
-				
-				File file = new File (WEB_ROOT, METHOD_NOT_SUPPORTED);
-				int fileLength= (int) file.length();
-				//String contentMimeType = "text/html";
-				
-				byte[] fileData = readFileData(file,fileLength);
-				
-				out.println("HTTP/1.1 501");
-				
-				dataOut.write(fileData, 0, fileLength);
-				dataOut.flush();
-				
-			}else {
+
+			{
 				if(fileRequested.endsWith("/")) {
-					fileRequested+= DEFAULT_FILE;
+					fileRequested+= INDEX;
+					out.println("Pagina De Inicio");
 				}
-				
 				File file = new File(WEB_ROOT, fileRequested);
 				int fileLength= (int) file.length();
 				String content = getContentType(fileRequested);
-				
-				if(method.equals("GET")) {
-					byte [] fileData = readFileData(file,fileLength);
-					
-					out.println("HTTP/1.1 200 OK");
-					
-					dataOut.write(fileData, 0, fileLength);
-					dataOut.flush();
-				}
-				
-				if (verbose) {
-					System.out.println("File " + fileRequested + "of type index" + content );
-				}
+
+				byte [] fileData = readFileData(file,fileLength);
+
+				dataOut.write(fileData, 0, fileLength);
+				dataOut.flush();
 			}
-			
+
 		}catch(FileNotFoundException fnfe) {
 			try {
 				fileNotFound(out, dataOut, fileRequested);
 			}catch(IOException ioe) {
 				System.err.println(ioe.getMessage());
 			}
-			
+
 		} catch (IOException e) {
-			System.err.println("Server error xdddd : " + e);
-		}finally {
+			System.err.println("Server error: " + e);
+
+		} finally {
 			try {
 				in.close();
 				out.close();
@@ -132,35 +97,25 @@ public class httpserver implements Runnable {
 				System.err.println("Error closing : " + e.getMessage());
 			}
 			if(verbose) {
-			System.out.println("Conexion cerrada");
+				System.out.println("Conexion cerrada");
+			}
 		}
-		}
-		
-		
-		
 	}
 
 	private void fileNotFound(PrintWriter out, OutputStream dataOut, String fileRequested) throws IOException {
-		File file = new File(WEB_ROOT, ERROR_NOT_FOUND);
+		File file = new File(ERROR_NOT_FOUND);
 		int fileLength= (int) file.length();
-		//String content = "text/htm";
 		byte[] fileData = readFileData(file, fileLength);
-		
-		//out.println("HTTP/1.1 404 FILE NOT FOUND");
-		
+
+		//out.println("ERROR 404 ARCHIVO NO ENCONTRADO");
 		dataOut.write(fileData, 0, fileLength);
 		dataOut.flush();
-		
-		if (verbose) {
-			System.out.println("File" + fileRequested + "not found");
-		}
-		
 	}
-
+	
 	private byte[] readFileData(File file, int fileLength) throws IOException {
 		FileInputStream fileIn=null;
 		byte[] fileData = new byte[fileLength];
-		
+
 		try {
 			fileIn = new FileInputStream(file);
 			fileIn.read(fileData);
@@ -168,17 +123,14 @@ public class httpserver implements Runnable {
 			if(fileIn != null)
 				fileIn.close();
 		}
-		
+
 		return fileData;
 	}
-	
+
 	private String getContentType(String fileRequested) {
 		if(fileRequested.endsWith(".htm") || fileRequested.endsWith(".html"))
 			return "text/html";
 		else
 			return "text/plain";
-		
 	}
-
-
 }
